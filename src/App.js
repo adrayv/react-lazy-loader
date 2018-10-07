56aea2397164aded51830f8c35a24777f048bda5
Destin Raymundo <draym001@g.ucla.edu>
2018-10-07 16:47:33 -0700

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import LazyLoad from 'react-lazy-load';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+	  	<MyComponent/>
       </div>
     );
   }
 }
 
 export default App;
+
+ 
+const MyComponent = () => (
+  <div>
+    Scroll to load images.
+    <div className="filler" />
+    <LazyLoad height={762} offsetVertical={300}>
+      <img src='http://apod.nasa.gov/apod/image/1502/HDR_MVMQ20Feb2015ouellet1024.jpg' />
+    </LazyLoad>
+    <div className="filler" />
+    <LazyLoad height={683} offsetTop={200}>
+      <img src='http://apod.nasa.gov/apod/image/1502/2015_02_20_conj_bourque1024.jpg' />
+    </LazyLoad>
+    <div className="filler" />
+    <LazyLoad height={480} offsetHorizontal={50}>
+      <img src='http://apod.nasa.gov/apod/image/1502/MarsPlume_jaeschke_480.gif' />
+    </LazyLoad>
+    <div className="filler" />
+    <LazyLoad
+      height={720}
+      onContentVisible={() => console.log('look ma I have been lazyloaded!')}
+    >
+      <img src='http://apod.nasa.gov/apod/image/1502/ToadSky_Lane_1080_annotated.jpg' />
+    </LazyLoad>
+    <div className="filler" />
+  </div>
+);
